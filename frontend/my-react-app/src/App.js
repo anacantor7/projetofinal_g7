aa9dc031ad340ca44bb706bebb530ccb6cc9eccd
anacantor7 <anamariacantorguzman@gmail.com>
2025-06-17 21:30:06 -0300

    Agrega los últimos cambios

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-
+import Footer from './components/footer';
+import Header from './components/header';   
+import navbar from './components/navbar';
 
 const App = () => {
     return (
         <div>
+            <Header />
+            <navbar />
             <h1>Welcome to My React App</h1>
             <p>This is a simple React application.</p>
             <button>Learn More</button>
+            <Footer />
         </div>
     );
 };
---
+++ frontend/my-react-app/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const footer  = () => {
+    return (
+        <footer>
+            <p>&copy; 2023 My React App. All rights reserved.</p>
+            <a href="/privacy-policy">Privacy Policy</a>
+        </footer>
+    );
+}
+export default footer;
