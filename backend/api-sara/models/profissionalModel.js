e01955fcbd9c61f9c4592f76e9f997258344e8b3
sara <saraebrahimgeorge@gmail.com>
2025-06-20 16:26:32 -0300

    Atualiza modelo Profissional com campos obrigatórios e controle de visibilidade

--- backend/api-sara/models/profissionalModel.js
+++ backend/api-sara/models/profissionalModel.js
@@ -1,14 +1,18 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database/db');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/db");
 
-const Profissional = sequelize.define('Profissional', {
+const Profissional = sequelize.define("Profissional", {
   nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  telefone: {
     type: DataTypes.STRING,
     allowNull: false,
   },
   especialidade: {
     type: DataTypes.STRING,
-    allowNull: true, // حقل اختياري
+    allowNull: false,
   },
   ativo: {
     type: DataTypes.BOOLEAN,
