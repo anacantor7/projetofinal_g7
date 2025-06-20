369ce21d34001f4580e8f6e7d6c0da66f16124da
sara <saraebrahimgeorge@gmail.com>
2025-06-20 16:38:20 -0300

    Ajusta app.js para integrar rotas e configurações finais da API

--- backend/api-sara/app.js
+++ backend/api-sara/app.js
@@ -12,12 +12,13 @@ const tipoRoutes = require("./routes/tipoRoutes");
 
 const app = express();
 const sequelize = require("./database/db");
+
+// تحميل النماذج (Models)
 const Cliente = require("./models/clienteModel");
 const Servico = require("./models/servicoModel");
 const Tipo = require("./models/tipoModel");
 const Profissional = require("./models/profissionalModel");
 const Agendamento = require("./models/agendamentoModel");
-
 
 //  علاقات Sequelize
 // Cliente 1:N Agendamento
@@ -40,22 +41,22 @@ sequelize
   .sync()
   .then(() => {
     console.log("Banco de dados sincronizado com sucesso!");
+
+    app.use(cors());
+    app.use(express.json());
+
+    app.use("/clientes", clienteRoutes);
+    app.use("/servicos", servicoRoutes);
+    app.use("/tipos", tipoRoutes);
+    app.use("/profissionais", profissionalRoutes);
+    app.use("/agendamentos", agendamentoRoutes);
+
+    const PORT = process.env.PORT || 3000;
+    console.log("Ambiente:", process.env.NODE_ENV);
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
   })
   .catch((err) => {
     console.error("Erro ao sincronizar o banco de dados:", err);
   });
-
-app.use(cors());
-app.use(express.json());
-app.use("/clientes", clienteRoutes);
-app.use("/servicos", servicoRoutes);
-app.use("/tipos", tipoRoutes);
-app.use("/profissionais", profissionalRoutes);
-app.use("/agendamentos", agendamentoRoutes);
-
-const PORT = process.env.PORT || 3000;
-
-console.log("Ambiente:", process.env.NODE_ENV);
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
